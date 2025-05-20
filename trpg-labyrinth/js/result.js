1c96c4124adb0e6b398daf0be97e4a2a5a320188
LaeGyeo <66859742+TAG-WEB@users.noreply.github.com>
2025-05-20 23:36:04 +0900

    V5

--- trpg-labyrinth/js/result.js
+++ trpg-labyrinth/js/result.js
@@ -2,3 +2,12 @@ window.onload = () => {
   document.getElementById("final-hints").textContent = localStorage.getItem("finalHints") || 0;
   document.getElementById("final-time").textContent = localStorage.getItem("finalTime") || 0;
 };
+
+// ✅ 트위터 공유 기능
+function shareTwitter() {
+  const url = 'https://trpg-labrinth.vercel.app/index.html';
+  const time = document.getElementById("final-time").textContent;
+  const hints = document.getElementById("final-hints").textContent;
+  const text = `내 클리어: ${time}초\n힌트 사용: ${hints}회\n나보다 더 빨리 미궁탈출 가능한 사람?\nTRPG길드 설연 미궁게임은 요기!>`;
+  window.open(`https://x.com/intent/post?text=${encodeURIComponent(text)}&url=${encodeURIComponent(url)}`);
+}
